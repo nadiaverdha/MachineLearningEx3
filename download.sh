eb4b25dd0f52151bbc65bd81138a46eb0843a3e5
nadia <67629937+nadiaverdha@users.noreply.github.com>
2023-07-09 15:56:32 +0200

    updated w/ the new data link for first dataset

--- download.sh
+++ download.sh
@@ -1,9 +1,4 @@
 # download data files
-mkdir news_data
-curl -o news_data/20news-bydate.tar.gz http://qwone.com/~jason/20Newsgroups/20news-bydate.tar.gz
-gunzip -c news_data/20news-bydate.tar.gz | tar xopf - -C news_data
-
-
-mkdir reuters_data
-curl -o reuters_data/euters21578.tar.gz http://www.daviddlewis.com/resources/testcollections/reuters21578/reuters21578.tar.gz
-gunzip -c reuters_data/euters21578.tar.gz | tar xopf - -C reuters_data
+mkdir cifar
+curl -o cifar/cifar-10-python.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+gunzip -c cifar/cifar-10-python.tar.gz | tar xopf - -C cifar
